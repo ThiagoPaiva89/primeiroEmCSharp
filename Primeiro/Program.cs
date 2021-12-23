9a0caa7fb27be3def17b6982f1f5ab868328612f
ThiagoPaiva89 <thiagoinaciopaiva@hotmail.com>
2021-12-23 14:00:38 -0300

    Commit de projeto teste em C#

---
+++ Primeiro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = DateTime.UtcNow;
+            DateTime d3 = DateTime.Today;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+
+        }
+    }
+}
